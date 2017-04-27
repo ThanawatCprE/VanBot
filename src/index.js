8a89c8e54e0bed699343536ef462eb1b4408d372
ToshyraF <faryhost@gmail.com>
2017-04-27 19:41:18 +0700

    test get start

--- src/index.js
+++ src/index.js
@@ -202,32 +202,32 @@ function callSendAPI(messageData) {
   });
 }
 
-// function callThreadSettingsAPI(data) { //Thread Reference API
-// request({
-// uri: 'https://graph.facebook.com/v2.6/me/thread_settings',
-// qs: { access_token: access },
-// method: 'POST',
-// json: data
-//
-// }, function (error, response, body) {
-// if (!error && response.statusCode == 400) {
-//   console.log("Thread Settings successfully changed!");
-// } else {
-//   console.error("Failed calling Thread Reference API", response.statusCode, response.statusMessage, body.error);
-// }
-// });
-// }
-//
-// function createGetStarted() {
-// var data = {
-//   "setting_type":"greeting",
-//   "greeting":{
-//     "text":"Hi {{user_first_name}}, welcome to this bot."
-//   }
-// };
-// callThreadSettingsAPI(data);
-// }
+ function callThreadSettingsAPI(data) { //Thread Reference API
+ request({
+ uri: 'https://graph.facebook.com/v2.6/me/thread_settings',
+ qs: { access_token: access },
+ method: 'POST',
+ json: data
+
+ }, function (error, response, body) {
+ if (!error && response.statusCode == 400) {
+   console.log("Thread Settings successfully changed!");
+ } else {
+   console.error("Failed calling Thread Reference API", response.statusCode, response.statusMessage, body.error);
+ }
+ });
+ }
+
+ function createGetStarted() {
+ var data = {
+   "setting_type":"greeting",
+   "greeting":{
+     "text":"Hi {{user_first_name}}, welcome to this bot."
+   }
+ };
+ callThreadSettingsAPI(data);
+ }
 app.listen(app.get('port'),function(){
-	// createGetStarted();
+	 createGetStarted();
 	console.log('runing on port',app.get('port'))
 })
