4290cda6dd91944892056d9a0f688cd8163f23c3
ToshyraF <faryhost@gmail.com>
2017-05-01 18:52:54 +0700

    show time

--- src/index.js
+++ src/index.js
@@ -87,7 +87,7 @@ function receivedMessage(event) {
   var recipientID = event.recipient.id;
   var timeOfMessage = event.timestamp;
   var message = event.message;
-  console.log(timeOfMessage);
+  console.log("time-----------:"+timeOfMessage);
   // console.log("Received message for user %d and page %d at %d with message:",
   //   senderID, recipientID, timeOfMessage);
   // console.log(JSON.stringify(message));
