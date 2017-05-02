7f4cd38d3f7da29ec9e6c05a1f88c5bd8cceb190
ToshyraF <faryhost@gmail.com>
2017-05-03 03:17:30 +0700

    sadf

--- src/index.js
+++ src/index.js
@@ -238,19 +238,15 @@ function callSendAPI(messageData) {
   });
 }
 
-var client = new pg.Client({
-	user: "ifeygszgzemhgc",
-	password: "c6500d57a0d859b425fbf6808052bcf2d0955da468aa90ff069c6c9c85cc536f",
-	database: "djmi984ka9f4r",
-	port: 5432,
-	host: "ec2-23-23-111-171.compute-1.amazonaws.com",
-	ssl: true
-});
-function connectPost(){
-
-
-}
 function Querydata(temp){
+  var client = new pg.Client({
+    user: "ifeygszgzemhgc",
+    password: "c6500d57a0d859b425fbf6808052bcf2d0955da468aa90ff069c6c9c85cc536f",
+    database: "djmi984ka9f4r",
+    port: 5432,
+    host: "ec2-23-23-111-171.compute-1.amazonaws.com",
+    ssl: true
+  });
   client.connect();
   client.query("select * from cdetail where rcompany ='"+temp+"';",function(err,rows,fields){
     if (err) throw err;
